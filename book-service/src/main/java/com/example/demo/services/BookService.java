f24206efbd39668fd1680625cc28be92ce20d2fd
dhanubharathi <dhanubharathi10@gmail.com>
2021-10-07 12:14:15 +0530

    REQ-103 -Created the book service as per the requirement

---
+++ book-service/src/main/java/com/example/demo/services/BookService.java
@@ -0,0 +1,29 @@
+package com.example.demo.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.Book;
+
+@Service
+
+
+public class BookService {
+	
+	@Autowired
+	private Book book;
+	public Book getBookById(int id)
+	{
+		book.setBookId(202);
+		book.setBookName("Head First java");
+		book.setAuthor("kathy sieera");
+		if(id==1)
+		{
+			book.setBookId(203);
+			book.setBookName("Effective java");
+			book.setAuthor("Bruce Erickel");
+		}
+		return this.book;
+	}
+
+}
